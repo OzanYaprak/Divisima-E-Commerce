a94b0ee1e0b5ed10ba80cadeaea4f8fc4cdb0f58
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2023-09-03 12:47:31 +0300

    Clear 03.09

--- Divisima/Program.cs
+++ Divisima/Program.cs
@@ -15,38 +15,8 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 });
 var app = builder.Build();
 if (!app.Environment.IsDevelopment()) app.UseStatusCodePagesWithRedirects("/hata/{0}");
-//app.Run(async context => await context.Response.WriteAsync("selam"));
 
-//app.Use(async (context, next) =>
-//{
-//    await context.Response.WriteAsync("Middleware 1");
 
-//    await next.Invoke();
-
-//    await context.Response.WriteAsync("Middleware 2");
-//});
-
-//app.Map("/test", app =>
-//    app.Run(async context =>
-//    {
-//        await context.Response.WriteAsync("Middleware 1");
-//    }));
-
-//app.Use(async (context, next) =>
-//{
-//    var url = context.Request.Path.Value;
-//    if (url.Contains("/infotech"))
-//    {
-//        context.Response.Redirect("https://www.infotechacademy.com.tr");
-//        return;
-//    }
-//    await next();
-//});
-
-//app.MapWhen(x => x.Request.Method == "GET" && !x.Request.IsHttps && x.Request.Path.Value.Contains("/abc"), app =>
-//{
-//    app.Run(async context => await context.Response.WriteAsync("Middleware 2"));
-//});
 
 
 
